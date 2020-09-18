baff4f0185e422b42f9d8c1d79f6991a128257e3
Manish Singh <astromanishsingh@gmail.com>
2020-09-19 01:34:16 +0530

    route for answer added

---
+++ routes/answer.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const Answer = require("./../models/answer");
+const router = express.Router();
+
+router.get("/:ques_id", async (req, res) => {
+  try {
+    const answers = await Answer.find({ ques_id: req.params.ques_id });
+    res.json(answers);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.post("/:ques_id", async (req, res) => {
+  const answer = new Answer({
+    a_body: req.body.a_body,
+    writer_name: req.body.writer_name,
+    ques_id: req.params.ques_id,
+  });
+  try {
+    const savedAns = await answer.save();
+    res.json(savedAns);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.patch("/:ques_id/:ans_id", async (req, res) => {
+  try {
+    const updatedAns = await Answer.updateOne(
+      {
+        ques_id: req.params.ques_id,
+        _id: req.params.ans_id,
+      },
+      { $set: { a_body: req.body.a_body } }
+    );
+    res.json(updatedAns);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.delete("/:ques_id/:ans_id", async (req, res) => {
+  try {
+    const deletedAns = await Answer.remove({
+      ques_id: req.params.ques_id,
+      _id: req.params.ans_id,
+    });
+    res.json(deletedAns);
+  } catch (err) {}
+});
+module.exports = router;
